9232a9e5610cdc7d35558ca1786e931faf450b30
Bansi <bansivachhani153@gmail.com>
2025-07-09 21:20:04 +0530

    added about section module

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,11 +47,12 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        I'm a passionate full-stack web developer skilled in JavaScript,
-        and modern frameworks like React, Node.js, and Express. I
-        specialize in building responsive frontends, RESTful APIs, and
-        database-driven applications using MongoDB and MySQL.I enjoy solving real-world
-        problems and collaborating on impactful, scalable solutions.
+        I'm a passionate full-stack web developer skilled in JavaScript, and
+        modern frameworks like React, Node.js, and Express. I specialize in
+        building responsive frontends, RESTful APIs, and database-driven
+        applications using MongoDB and MySQL.I enjoy solving real-world problems
+        and collaborating on impactful, scalable solutions.Let's work together
+        to bring your ideas to life!
       </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10">
