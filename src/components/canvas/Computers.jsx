6662737766cc0b39c687891bca4377325b03d111
Bansi <bansivachhani153@gmail.com>
2025-07-09 18:29:42 +0530

    addded a loading of 3d computer

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,10 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+ // const computer = useGLTF("../desktop_pc/scene.gltf")
+ const computer = useGLTF("/desktop_pc/scene.gltf");
+// const computer = useGLTF("https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Duck/glTF/Duck.gltf");
+
 
   return (
     <mesh>
